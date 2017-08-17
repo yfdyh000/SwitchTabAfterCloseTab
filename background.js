3b89eaf63060a251d90fdde6590b029809ee41cd
YFdyh000 <yfdyh000@gmail.com>
2017-08-18 00:36:18 +0800

    Detect "Open Tabs Next to Current" and delay rememberTabs

--- background.js
+++ background.js
@@ -1,8 +1,15 @@
 let currentWindowTabs;
+let delayOnCreated = 0;
 
 async function rememberTabs() {
     currentWindowTabs = await browser.tabs.query({ currentWindow: true });
     return currentWindowTabs;
+}
+
+async function delay_rememberTabs() {
+    setTimeout(function () {
+        rememberTabs();
+    }, delayOnCreated); // compatible with other add-ons, like "Open Tabs Next to Current"
 }
 
 async function tabRemoved(tabId, removeInfo) {
@@ -14,10 +21,20 @@ async function tabRemoved(tabId, removeInfo) {
     rememberTabs();
 }
 
+async function detectConflictAddons() {
+    browser.management.get("opentabsnexttocurrent@sblask").then((ExtensionInfo) => {
+        if (!ExtensionInfo.enabled) return;
+        console.warn(`"Open Tabs Next to Current" is enabled, attempt delay to avoid conflicts.`)
+        delayOnCreated = 500; // TODO: Any more reliable ways? Or make it customizable is needed?
+    });
+}
+
 rememberTabs();
+detectConflictAddons();
 
 browser.windows.onFocusChanged.addListener(rememberTabs);
 browser.windows.onRemoved.addListener(rememberTabs);
 browser.tabs.onActivated.addListener(rememberTabs);
-browser.tabs.onCreated.addListener(rememberTabs);
+browser.tabs.onCreated.addListener(delay_rememberTabs);
 browser.tabs.onRemoved.addListener(tabRemoved);
+browser.management.onEnabled.addListener(detectConflictAddons);
